943ef46378648a5546a9f36700e9ed2fe42de897
soonduck <nederileni99heits@gmail.com>
2025-05-23 00:52:49 +0900

    refactor: MVVM 및 커맨드 패턴을 위한 리팩토링 중 2

--- src/main/java/cose457/drawingtool/view/viewmodelvisitor/ShapeViewModelVisitor.java
+++
@@ -1,7 +0,0 @@
-package cose457.drawingtool.view.viewmodelvisitor;
-
-import cose457.drawingtool.viewmodel.RectangleShapeViewModel;
-
-public interface ShapeViewModelVisitor {
-    void visit(RectangleShapeViewModel viewModel);
-}
